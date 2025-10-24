41688889da3a3664a115d488edf612c6b9b7bada
thariq.aziz <apoban1234@gmail.com>
2025-10-25 01:40:13 +0700

    refactor: reorganize imports and enhance server startup logging

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
+import express, { Request, Response } from "express"; // Mengimpor framework Express
 
-dotenv.config();
+require("dotenv").config(); // Memuat variabel lingkungan dari file .env ke process.env
+const cors = require("cors"); // Mengimpor middleware CORS
+const app = express(); // Membuat instance aplikasi Express
 
-const app = express();
+app.use(cors()); // Mengaktifkan CORS (Cross-Origin Resource Sharing) untuk semua rute
 
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 8000; // Mengatur port server dari variabel lingkungan atau default ke 8000
 
-const PORT = process.env.PORT || 8000;
+app.use(express.json()); // Mengurai permintaan JSON yang masuk dan meletakkan data di req.body
 
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
+  // Menjalankan server dan mendengarkan pada port yang telah ditentukan
+  console.log(`Server is running on http://localhost:${PORT}`); // Menampilkan pesan bahwa server berjalan
+});
+
+app.get("/customers", (req: Request, res: Response) => {
+  const customers = [
+    { name: "John Doe", email: "john.doe@example.com", phone: "+1234567890" },
+    {
+      name: "Joel Smith",
+      email: "joel.smith@example.com",
+      phone: "+0987654321",
+    },
+  ];
+
+  return res.status(200).json(customers);
 });
